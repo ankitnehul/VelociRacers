857d813f66b0a506280bbbdf807931c385f66b36
AnkitNehul <ankitnehul23012gmail.com>
2022-02-23 01:51:47 +0530

    modified some files

---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+import React, {useState}from 'react'
+
+function navbar() {
+  return (
+   <>
+   //nav.navbar
+   <nav className="navbar"></nav>
+    <div className="navbar-container"></div>
+   <Link className='navbar-logo'>abcd</Link>
+   </>
+  )
+}
+
+export default navbar
